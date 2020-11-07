dea076cbe3bfffe93e3639c41f16470814d84c21
golfdcd <68695900+golfdcd@users.noreply.github.com>
2020-11-07 16:46:05 +0700

    mobile2

--- app/src/main/java/com/example/ggshop/MainActivity.kt
+++ app/src/main/java/com/example/ggshop/MainActivity.kt
@@ -2,10 +2,50 @@ package com.example.ggshop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.login.*
+import kotlinx.android.synthetic.main.user_registration.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var handler:DatabaseHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        handler= DatabaseHelper(this)
+
+        showHome()
+
+        registration.setOnClickListener{
+            showRegistration()
+        }
+        login.setOnClickListener {
+            showLogIn()
+        }
+
+        save.setOnContextClickListener {
+            handler.insertUserData(name.text.toString(),email.text.toString().password_register.text.toString())
+        }
+    }
+
+    private fun showRegistration(){
+        resgistration_layout.visibility=View.VISIBLE
+        login_layout.visibility=View.GONE
+        home_11.visibility=View.VISIBLE
+    }
+
+    private fun showLogIn() {
+        resgistration_layout.visibility = View.VISIBLE
+        login_layout.visibility = View.GONE
+        home_11.visibility = View.VISIBLE
+    }
+
+    private fun showHome(){
+        resgistration_layout.visibility = View.VISIBLE
+        login_layout.visibility = View.GONE
+        home_11.visibility = View.VISIBLE
     }
 }
